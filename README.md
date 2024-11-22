b118e908f6bbcd0ae8285a39a2d563bb5966e868
Youssef El-Hamrani <yel-hamr@c3r7p5.1337.ma>
2024-11-22 14:14:33 +0100

    version 1.0.1

---
+++ README.md
@@ -0,0 +1,37 @@
+if (str[i] == '%')
+{
+	conversions : c s p d i u x X %
+	precesions : - 0 . # (space) +
+}
+
+
+
+
+- left alignment :
+	applies for c s p d i u x X %
+	Left-aligns the output within the specified field width.
+
+0 Zero padding :
+	applies to d i u x X %
+	Pads the output with zeros instead of spaces.
+
+. Precision :
+	applies for d i u x X
+	Sets the minimum number of digits. Pads with leading zeros if necessary.
+	applies for s
+	Limits the maximum number of characters to display from the string.
+
+
+# Alternative form :
+	applies for x X
+	Adds a prefix:
+	0x for lowercase hexadecimal (x).
+    0X for uppercase hexadecimal (X).
+
++ Force sign :
+	applies for d i
+	Forces the sign to always appear (+ or -).
+
+  Space :
+	applies for d i
+	Adds a leading space for positive numbers (if + is not used).
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yel-hamr <yel-hamr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 10:40:55 by yel-hamr          #+#    #+#             */
-/*   Updated: 2024/11/22 12:33:59 by yel-hamr         ###   ########.fr       */
+/*   Updated: 2024/11/22 14:10:57 by yel-hamr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ int	flag_traitement(const char *format, int *flags, int i, int *width, int *prec
 			*precision = ft_atoi(&format[i]);
 			while (format[i] >= '0' && format[i] <= '9')
 				i++;
+			i--;
 		}
 		else if (format[i] == '#')
 			flags[3] = 1;
@@ -60,6 +61,7 @@ int	flag_traitement(const char *format, int *flags, int i, int *width, int *prec
 			*width = ft_atoi(&format[i]);
 			while (format[i] >= '0' && format[i] <= '9')
 				i++;
+			i--;
 		}
 		else
 			break;
@@ -70,8 +72,19 @@ int	flag_traitement(const char *format, int *flags, int i, int *width, int *prec
 
 void	conversions_traitement(char c, va_list args, int *count, int *flags, int width, int precision)
 {
+	int	temp;
+	int	i;
+
 	if (c == 'c')
+	{
 		ft_putchar_fd(va_arg(args, int), 1);
+		if (flags[0] == 1)
+		{
+			i = 0;
+			while (i++ < width - 1)
+				ft_putchar_fd(' ', 1);
+		}
+	}
 	else if (c == 's')
 		ft_putstr_fd(va_arg(args, char *), 1);
 	else if (c == 'p')
@@ -85,16 +98,29 @@ void	conversions_traitement(char c, va_list args, int *count, int *flags, int wi
 		else
     	    ft_putstr_fd("(nil)", 1);
 	}
-	else if (c == 'd')
-		ft_putnbr_fd(va_arg(args, int), 1);
-	else if (c == 'i')
-		ft_putnbr_fd(va_arg(args, int), 1);
+	else if (c == 'd' || c == 'i')
+	{
+		temp = va_arg(args, int);
+		if (temp >= 0 && flags[5] == 1)
+			ft_putchar_fd('+', 1);
+		else if (temp >= 0 && flags[4] == 1)
+			ft_putchar_fd(' ', 1);
+		ft_putnbr_fd(temp, 1);
+	}
 	else if (c == 'u')
 		ft_put_unsigned_nbr_fd(va_arg(args, unsigned int), 1);
 	else if (c == 'x')
+	{
+		if (flags[3] == 1)
+			ft_putstr_fd("0x", 1);
 		print_hex_lower(va_arg(args, int));
+	}
 	else if (c == 'X')
+	{
+		if (flags[3] == 1)
+			ft_putstr_fd("0X", 1);
 		print_hex_upper(va_arg(args, int));
+	}
 	else if (c == '%')
 		ft_putchar_fd('%', 1);
 }
@@ -123,7 +149,7 @@ int	ft_printf(const char *format, ...)
 			if (format[i] == '-' || format[i] == '0' || format[i] == '.' 
 				|| format[i] == '#' || format[i] == ' ' || format[i] == '+')
 				i = flag_traitement(format, flags, i, &width, &precision);
-			else if (format[i] == 'c' || format[i] == 's' || format[i] == 'p' 
+			if (format[i] == 'c' || format[i] == 's' || format[i] == 'p' 
 				|| format[i] == 'd' || format[i] == 'i' || format[i] == 'u' 
 				|| format[i] == 'x' || format[i] == 'X')
 				conversions_traitement(format[i], args, &count, flags, width, precision);
@@ -138,12 +164,28 @@ int	ft_printf(const char *format, ...)
 	return (count);
 }
 
+
+/*
 int main ()
 {
-	const char *format = "%-#8.5d|\n";
-	int flags[6];
+	const char *format = "%#X|\n";
+	int flags[6]; // - 0 . # space +
+	ft_memset(flags , 0, sizeof(flags));
 	int width = 0;
 	int precision = 0;
 	int ret = flag_traitement(format, flags, 1, &width, &precision);
-	printf("width is %d, ans precision is %d\n", width, precision);
+	printf("width is %d, precision is %d and i is %d\n", width, precision, ret);
+	for (int j = 0; j < 6 ; j++)
+	{
+		printf("%d", flags[j]);
+	}
+	printf("\n");
+}
+*/
+int main ()
+{
+	int a = 5;
+	int *b = &a;
+	ft_printf("This is a test |%-5c|\n", 'a');
+	printf("This is a test |%-15p|\n", b);
 }
