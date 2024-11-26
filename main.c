5b59ef3535d57e4a6515bdd40133a9da12b8bdb5
Youssef El-Hamrani <yel-hamr@c3r8p5.1337.ma>
2024-11-26 13:12:29 +0100

    version 4.0.1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "ft_printf.h"
+
+int main ()
+{
+	int a = 5;
+	int *a = 
+	ft_printf("This is a test |%p|\n",);
+	printf("This is a test |%p|\n",'v');
+}
+
+/*
+int main ()
+{
+	ft_printf("This is a test |%-15.15d|\n",255);
+	printf("This is a test |%-15.15d|\n",255);
+}
+*/
